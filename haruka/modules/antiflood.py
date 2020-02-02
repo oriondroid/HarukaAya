8fa86e26103812f21e73a0bee4321d3125314398
oriondroid <60574228+oriondroid@users.noreply.github.com>
2020-02-02 19:44:23 +0530

    Update antiflood.py

--- haruka/modules/antiflood.py
+++ haruka/modules/antiflood.py
@@ -37,8 +37,7 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     try:
         bot.restrict_chat_member(chat.id, user.id, can_send_messages=False)
-        msg.reply_text(tld(chat.id, "I like to leave the flooding to natural disasters. But you, you were just a "
-                       "disappointment. *Muted*!"))
+        msg.reply_text(tld(chat.id, "Done playing with you freaks! *Muted*!"))
 
         return "<b>{}:</b>" \
                "\n#MUTED" \
@@ -47,7 +46,7 @@ def check_flood(bot: Bot, update: Update) -> str:
                                              mention_html(user.id, user.first_name))
 
     except BadRequest:
-        msg.reply_text(tld(chat.id, "I can't mute people here, give me permissions first! Until then, I'll disable antiflood."))
+        msg.reply_text(tld(chat.id, "Insufficient permissions! Until then, I'll disable antiflood."))
         sql.set_flood(chat.id, 0)
         return "<b>{}:</b>" \
                "\n#INFO" \
@@ -85,7 +84,7 @@ def set_flood(bot: Bot, update: Update, args: List[str]) -> str:
 
             else:
                 sql.set_flood(chat.id, amount)
-                message.reply_text(tld(chat.id, "Antiflood has been updated and set to {}").format(amount))
+                message.reply_text(tld(chat.id, "Antiflood has been updated to {}").format(amount))
                 return "<b>{}:</b>" \
                        "\n#SETFLOOD" \
                        "\n<b>Admin:</b> {}" \
@@ -120,7 +119,7 @@ def __chat_settings__(bot, update, chat, chatP, user):
     if limit == 0:
         return "*Not* currently enforcing flood control."
     else:
-        return "Antiflood is set to `{}` messages.".format(limit)
+        return "Antiflood updated to `{}` messages.".format(limit)
 
 
 __help__ = """
